96aa4e5ba41437404052beda9ea59028ac3e19fb
Varun Jayaraman <varun.jayaraman@gmail.com>
2017-06-20 00:30:12 -0400

    begin

--- src/server/api/search/routes.ts
+++ src/server/api/search/routes.ts
@@ -7,16 +7,10 @@ import { findFromQuery } from './helpers';
 
 export const search = async (req, res) => {
   try {
-    console.log(req.query);
-
     const venues = await findFromQuery({ query: req.params, user: req.user });
 
     res.json({ venues });
   } catch (error) {
-    return res.status(500).json({
-      error,
-      stack: error.stack,
-      endpoint: `search.search`
-    });
+    return res.status(500).json({ error });
   }
 };
